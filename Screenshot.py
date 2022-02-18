2fab4670fdd2fe929c59437d41437c3e5846cfc7
Pradhu007 <55123592+Pradhu007@users.noreply.github.com>
2022-02-18 22:25:49 +1100

    Delete Screenshot.py

--- Screenshot.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import os
-
-def victomscreenshot():
-
-
-      desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop\\')
-      victom_Screenshot = pyautogui.screenshot()
-      victom_Screenshot.save(desktop + "RAT.jpg")
